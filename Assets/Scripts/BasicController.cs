6a2fad51720638e0283cdb4f406b2e8dc3a0cc37
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-04-15 13:24:19 +0530

    complete project

---
+++ Assets/Scripts/BasicController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        Debug.Log("horizontal input: " + Input.GetAxis("Horizontal"));
+    }
+}
---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour
+{
+    public float spinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+    public bool DoSpin = true;
+    public bool DoMotion = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rotate along the y axis of the object
+        if(DoSpin)
+        gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+
+        //move up and down overtime
+        if(DoMotion)
+        gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+    }
+}
